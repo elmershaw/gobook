a0f09d64226cdbb5c4e0a2ffc8075e29c9ed81ab
Elmer SHAW <elmershaw@163.com>
2021-08-12 17:13:26 +0800

    finished ex7.12

---
+++ ch7/exercise7.11/main.go
@@ -0,0 +1,88 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"strconv"
+)
+
+func main() {
+	db := database{"shoes": 50, "socks": 5}
+	http.HandleFunc("/list", db.list)
+	http.HandleFunc("/price", db.price)
+	http.HandleFunc("/insert", db.insertFunc)
+	http.HandleFunc("/select", db.selectFunc)
+	http.HandleFunc("/update", db.updateFunc)
+	http.HandleFunc("/delete", db.deleteFunc)
+	log.Fatal(http.ListenAndServe("localhost:8000", nil))
+}
+
+type dollars float32
+
+func (d dollars) String() string { return fmt.Sprintf("$%.2f", d) }
+
+type database map[string]dollars
+
+func (db database) list(w http.ResponseWriter, req *http.Request) {
+	for item, price := range db {
+		fmt.Fprintf(w, "%s: %s\n", item, price)
+	}
+}
+
+func (db database) price(w http.ResponseWriter, req *http.Request) {
+	item := req.URL.Query().Get("item")
+	price, ok := db[item]
+	if !ok {
+		w.WriteHeader(http.StatusNotFound) // 404
+		fmt.Fprintf(w, "no such item: %q\n", item)
+		return
+	}
+	fmt.Fprintf(w, "%s\n", price)
+}
+
+func (db database) insertFunc(w http.ResponseWriter, req *http.Request) {
+	item := req.URL.Query().Get("item")
+	price, err := strconv.ParseFloat(req.URL.Query().Get("price"), 32)
+	if err != nil {
+		log.Print(err)
+	}
+	db[item] = dollars(price)
+}
+
+func (db database) updateFunc(w http.ResponseWriter, req *http.Request) {
+	item := req.URL.Query().Get("item")
+	_, ok := db[item]
+	if !ok {
+		w.WriteHeader(http.StatusNotFound) // 404
+		fmt.Fprintf(w, "no such item: %q\n", item)
+		return
+	}
+	price, err := strconv.ParseFloat(req.URL.Query().Get("price"), 32)
+	if err != nil {
+		log.Print(err)
+	}
+	db[item] = dollars(price)
+}
+
+func (db database) selectFunc(w http.ResponseWriter, req *http.Request) {
+	item := req.URL.Query().Get("item")
+	price, ok := db[item]
+	if !ok {
+		w.WriteHeader(http.StatusNotFound) // 404
+		fmt.Fprintf(w, "no such item: %q\n", item)
+		return
+	}
+	fmt.Fprintf(w, "%s\n", price)
+}
+
+func (db database) deleteFunc(w http.ResponseWriter, req *http.Request) {
+	item := req.URL.Query().Get("item")
+	_, ok := db[item]
+	if !ok {
+		w.WriteHeader(http.StatusNotFound) // 404
+		fmt.Fprintf(w, "no such item: %q\n", item)
+		return
+	}
+	delete(db, item)
+}
